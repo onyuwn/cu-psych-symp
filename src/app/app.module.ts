a1760f9df80e97d0d997bb2191558c05974e6cc1
onyuwn <jakeherman@outlook.com>
2023-03-02 00:56:50 -0600

    Build base components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './base/header/header.component';
+import { JumbotronComponent } from './base/jumbotron/jumbotron.component';
+import { HomeComponent } from './home/home.component';
+import { SmallContentComponent } from './base/small-content/small-content.component';
+import { ImageContainerComponent } from './base/image-container/image-container.component';
+import { FooterComponent } from './base/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JumbotronComponent,
+    HomeComponent,
+    SmallContentComponent,
+    ImageContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/base/small-content/small-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-content',
+  templateUrl: './small-content.component.html',
+  styleUrls: ['./small-content.component.css']
+})
+export class SmallContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
